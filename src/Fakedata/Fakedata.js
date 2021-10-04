1d0fc890f8c443b6f41d0c6e2bba04a26fbdc614
sumaiyatasnim <tasnimsumaiya07@gmail.com>
2021-10-03 23:01:10 -0700

    commit

---
+++ src/Fakedata/Fakedata.js
@@ -0,0 +1,83 @@
+const data = [
+    {
+        id: 1,
+        style: "Hip Hop ",
+        instructor: "Kara Kannela",
+        classes: "10 Total ",
+        description: "Hip-hop dance is a fusion dance genre that incorporates elements of popping, locking, breaking, jazz, ballet, tap dancing and other styles and is typically performed to hip-hop, R&B, funk, electronic or pop music. ",
+        price: "300 ",
+        img: "https://www.vippng.com/png/detail/4-47619_banner-transparent-hip-hop-dance-png-black-and.png "
+    },
+    {
+        id: 2,
+        style: "Contemporary ",
+        instructor: "Anna Rose",
+        classes: "12 Total ",
+        description: "Contemporary dance is a style of expressive dance that combines elements of several dance genres including modern, jazz, lyrical and classical ballet. Contemporary dancers strive to connect the mind and the body through fluid dance movements. ",
+        price: "250 ",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmC67N5M6hit7sCWF5CJBtT5CfugbeI9r7TA&usqp=CAU "
+    },
+    {
+        id: 3,
+        style: "Locking",
+        instructor: "Jack Dawson",
+        classes: "9 Total ",
+        description: " Locking is a style of funk dance, which is today also associated with hip hop. The name is based on the concept of locking movements, which means freezing from a fast movement and locking in a certain position, holding that position for a short while and then continuing at the same speed as before. It relies on fast and distinct arm and hand movements combined with more relaxed hips and legs. The movements are generally large and exaggerated, and often very rhythmic and tightly synced with the music. Locking is performance oriented, often interacting with the audience by smiling or giving them a high five, and some moves are quite comical.",
+        price: "350 ",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmbJoCx_IaaFifZMt0WA_KIB6dQNVlscqZIQ&usqp=CAU "
+    },
+    {
+        id: 4,
+        style: "House ",
+        instructor: "John Ross",
+        classes: "10 Total ",
+        description: "House dance is a freestyle street dance and social dance that has roots in the underground house music scene of Chicago and New York. It is typically danced to loud and bass-heavy electronic dance music provided by DJs in nightclubs or at raves. ",
+        price: "200 ",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRX9xAHh8OK1vzsxQN5WrwICxCKpGNd9MV60g&usqp=CAU "
+    },
+    {
+        id: 5,
+        style: " Ballet",
+        instructor: "Emma Watts",
+        classes: "12 Total ",
+        description: "Ballet is an art form created by the movement of the human body. It is theatrical – performed on a stage to an audience utilizing costumes, scenic design and lighting. It can tell a story or express a thought, concept or emotion. Ballet dance can be magical, exciting, provoking or disturbing. ",
+        price: "275 ",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0kWHevvYbKJOShrEWo0QIZWEuyRDCrVhynw&usqp=CAU "
+    },
+    {
+        id: 6,
+        instructor: "Kiara Shell",
+        style: "Popping ",
+        classes: "15 Total ",
+        description: "Popping is a street dance adapted out of the earlier Boogaloo cultural movement in Oakland, California. As Boogaloo spread, it would be referred to as Robottin in Richmond, California, Strutting movements in San Francisco and San Jose, and the Strikin dances of the Oak Park community of Sacramento which were popular through the mid-1960s to the 1970s.[1][2] Popping would be eventually adapted from earlier Boogaloo movements in Fresno, California in the late 1970s by way of California high-school gatherings of track & meet events - the West Coast Relays. ",
+        price: "250 ",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9EgUo4eGYrlHGjjJ66WcHW1-_2tS1nSy3wA&usqp=CAU "
+    },
+    {
+        id: 7,
+        instructor: "Kenneth &  Steven",
+        style: "Jazz  ",
+        classes: "12 Total ",
+        description: " Jazz dance is a performance [dance] and style that in the United States arose in the mid 20th century. ... The two types expand on African-American vernacular styles of dance that arose with jazz music. Vernacular jazz dance incorporates ragtime moves, Charleston, Lindy hop and mambo.",
+        price: "300 ",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvTInvy1OciUoz7uxmCnR15c0AEFjZ4W6LBg&usqp=CAU "
+    },
+    {
+        id: 8,
+        instructor: "Thomas William",
+        style: "Heels ",
+        classes: " 12 Total",
+        description: "Heels dance is a dance form that emerged and evolved in the United States and Europe in the late 20th and early 21st centuries. It is named after the women's shoe style, since one of its distinguishing features is the wearing of high-heeled shoes during performance. ... It is also used in some go-go dancing performances. ",
+        price: "300 ",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3__wy7DQmR7KfEYsG4gmCy8FUSF-KvD90fw&usqp=CAU "
+    },
+    {
+        id: 9,
+        style: "Dance Workout ",
+        instructor: "Richard & Brian",
+        classes: "10 Total ",
+        description: " Simply put, dance cardio is utilizing different types of dance to exercise your body. The term “cardio” means working to achieve a target heart rate, increasing metabolic rate, burning calories, and improving stamina.",
+        price: "200 ",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfupLC-1PT2GcjnbisrNdXrjo2wmd2pTdDIg&usqp=CAU "
+    }
+]
